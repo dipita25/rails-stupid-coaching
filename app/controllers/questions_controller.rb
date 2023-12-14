6810f4ca4dfb2429296386cfcbc50f15299191e8
jonathan Dipita <jonathandipita@gmail.com>
2023-12-14 07:43:36 +0400

    in progress

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,14 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @answer = 'reponse normale'
+    @answer = '';
+    if params['question'] == "I am going to work"
+      @answer = 'Great!'
+      puts params['question']
+    elsif params['question'].end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
